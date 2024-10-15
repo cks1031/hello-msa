6a1f2214dfd606b0b75e1c6772e15fdd270faaa2
cks1031 <cks1031@naver.com>
2024-10-15 15:32:43 +0900

    user service - 회원가입 처리

--- msa-user-service/service/database.py
+++ msa-user-service/service/database.py
@@ -8,7 +8,7 @@ engine = sqlalchemy.create_engine(db_url, echo=True)
 SessionLocal = sessionmaker(autocommit=False,autoflush=False, bind=engine)
 
 def create_tales():
-    Base.metadata.create_all(bind=engine)
+    Base.metadata.create_all(engine)
 
 def get_db():
     with SessionLocal() as db:

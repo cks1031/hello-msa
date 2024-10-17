b3529cd361b4d283c3b7ef09ef11f0a8512cb182
cks1031 <cks1031@naver.com>
2024-10-17 14:00:39 +0900

    user service - 비밀번호 암호화 기능 추가

--- msa-user-service/service/auth.py
+++ msa-user-service/service/auth.py
@@ -1,7 +1,6 @@
 # JWT 로그인 처리
-from sqlalchemy import and_
+import bcrypt
 from sqlalchemy.orm import Session
-
 from models.user import User
 from schema.user import UserLogin, Token
 
@@ -19,3 +18,14 @@ def userlogin(login:UserLogin, db:Session):
         token = Token(access_token='hello', token_type="Bearer")
 
     return token
+
+# 비밀번호 암호화
+# bcrypt : 비밀번호 단방향 암호화에 자주 사용하는 패키지
+# 암호화 방법 : 사용자의 비밀번호 + bcryptd 고유한 솔트
+
+def hashed_password(passwd):
+    SALT = bcrypt.gensalt() # 솔트 생성
+    hashed_passwd = bcrypt.hashpw(passwd.encode('utf-8'), SALT)
+    print(hashed_passwd)
+
+    return hashed_passwd
--- msa-user-service/service/user.py
+++ msa-user-service/service/user.py
@@ -2,11 +2,17 @@
 from sqlalchemy.orm import Session
 from models.user import User
 from schema.user import UserBase
+from service.auth import hashed_password
+
 
 # 회원가입 처리
 # 기본 회원정보 + 번호, 가입일
 def register(db:Session, user: UserBase):
+    # 비밀번호 암호화
+    hashed_passwd = hashed_password(user.passwd)
+
     user = User(**user.model_dump())
+    user.passwd = hashed_passwd # 기본 비밀번호 교체
     db.add(user)
     db.commit()
     db.refresh(user)
